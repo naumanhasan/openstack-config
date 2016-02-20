cd29e0498eb3bc0c671e11587a4e7bca514391f5
naumanhasan <naumanhasan761@gmail.com>
2016-02-20 12:40:47 -0600

    Delete gitconfig.sh

--- gitconfig.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.name "naumanhasan"
-git config --global user.email naumanhasan761@gmail.com
-git config --global color.diff auto
-git config --global color.status auto
-git config --global color.branch auto
-
